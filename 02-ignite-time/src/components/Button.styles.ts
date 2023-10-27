1321f1e8670fc92165505c28454f5cac602f77d2
Filipe Alves <lipeqa@gmail.com>
2023-10-27 09:36:43 -0300

    React_ignite: estrutura da Aplicaçao - Cores & Fonte

--- 02-ignite-time/src/components/Button.styles.ts
+++ 02-ignite-time/src/components/Button.styles.ts
@@ -20,7 +20,7 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
     margin: 8px;
     border-radius: 4px;
 
-    background-color: ${props => props.theme.primary};
+    background-color: ${props => props.theme["green-500"]};
     color: ${props => props.theme.white};
 
     /* ${props => {
