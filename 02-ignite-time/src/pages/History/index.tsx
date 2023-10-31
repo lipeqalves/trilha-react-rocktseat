0aaf9c3421b0f4545f71bc2d5a9788ded4382a23
Filipe Alves <lipeqa@gmail.com>
2023-10-31 16:01:48 -0300

    React_ignite: Páginas e rotas - Componente Status

--- 02-ignite-time/src/pages/History/index.tsx
+++ 02-ignite-time/src/pages/History/index.tsx
@@ -1,9 +1,9 @@
-import { HistoryContainer, HistoryList } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
   return (
     <HistoryContainer>
-      <h1>History</h1>
+      <h1>Meu Histórico</h1>
       <HistoryList>
         <table>
           <thead>
@@ -19,25 +19,33 @@ export function History() {
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 mesed</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluído </Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 mesed</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="red">Interrompido </Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 mesed</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluído </Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 mesed</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="yellow">Em andamento </Status>
+              </td>
             </tr>
           </tbody>
         </table>
