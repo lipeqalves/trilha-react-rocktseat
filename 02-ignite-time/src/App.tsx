c518b76a7cba6032cc134790da117b546ef4f81f
Filipe Alves <lipeqa@gmail.com>
2023-11-07 09:23:54 -0300

    React_ignite: Contexto no React - Entendendo contexto

--- 02-ignite-time/src/App.tsx
+++ 02-ignite-time/src/App.tsx
@@ -1,15 +1,17 @@
-import { ThemeProvider } from 'styled-components'
-import { defaulTheme } from './styles/themes/default'
-import { GlobalStyle } from './styles/global'
-import { Router } from './Router'
-import { BrowserRouter } from 'react-router-dom'
+// import { ThemeProvider } from 'styled-components'
+// import { defaulTheme } from './styles/themes/default'
+// import { GlobalStyle } from './styles/global'
+// import { Router } from './Router'
+// import { BrowserRouter } from 'react-router-dom'
+import { Home } from './exemplo-contexto/Home'
 export function App() {
   return (
-    <ThemeProvider theme={defaulTheme}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-      <GlobalStyle />
-    </ThemeProvider>
+    // <ThemeProvider theme={defaulTheme}>
+    //   <BrowserRouter>
+    //     <Router />
+    //   </BrowserRouter>
+    //   <GlobalStyle />
+    // </ThemeProvider>
+    <Home />
   )
 }
